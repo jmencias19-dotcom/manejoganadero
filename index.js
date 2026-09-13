4f74d675d1a1edfd170e6ce314909da1df6f21f2
jmencias19-dotcom <jmencias19@gmail.com>
2026-09-13 12:59:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({ origin: '[https://manejoganadero.vercel.app](https://manejoganadero.vercel.app)' }));
 // Inicialización de Supabase
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
