d34c1a9ef0b841ca4d0ffb349fbc0824cf513108
jmencias19-dotcom <jmencias19@gmail.com>
2026-09-14 10:05:27 -0400

    Update historial-pastoreo.js

--- public/historial-pastoreo.js
+++ public/historial-pastoreo.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // SOLUCIÓN AL ERROR: Inicializar Firestore y definir la variable 'db'
 const db = getFirestore(app); 
