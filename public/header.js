6b16dd46732c190a8bc6e03175e10a5ed4cd1cc0
jmencias19-dotcom <jmencias19@gmail.com>
2026-09-18 10:56:36 -0400

    Update header.js

--- public/header.js
+++ public/header.js
@@ -4,15 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const headerHTML = `
         <header class="main-header">
             <div class="header-container">
-                <div class="flag-container">
-                    <img src="/img/venezuela.png" alt="Venezuela" class="flag-icon">
+                <div class="header-left">
+                    <a href="/index.html" class="btn-home" title="Panel Principal">
+                        <i class="fas fa-home"></i> Panel
+                    </a>
+                    <div class="flag-container">
+                        <img src="/img/venezuela.png" alt="Venezuela" class="flag-icon">
+                    </div>
                 </div>
+                
                 <div class="header-title-wrapper">
                     <h1>HATO LAGUNA BRAVA</h1>
                     <p>${tituloModulo}</p>
                 </div>
-                <div class="flag-container">
-                    <img src="/img/italia.png" alt="Italia" class="flag-icon">
+                
+                <div class="header-right">
+                    <div class="flag-container">
+                        <img src="/img/italia.png" alt="Italia" class="flag-icon">
+                    </div>
+                    <button id="btn-sync" class="btn-sync" title="Sincronizar datos">
+                        <i class="fas fa-sync-alt"></i> <span id="sync-status-text">Sincronizar</span>
+                    </button>
                 </div>
             </div>
         </header>
